31514b92b03fca7610646f356256d4b88c7769af
Ryan Francis <ryanfrancis@Ryans-MacBook-Pro.local>
2013-06-05 18:55:23 -0500

    updated wicked pdf initializer

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,3 +1,3 @@
 require 'wicked_pdf'
 require 'wicked_pdf_tempfile'
-WickedPdf.config = { :exe_path => '/usr/local/bin/wkhtmltopdf', :root_url => "http://localhost:3000" unless Rails.env.staging? || Rails.env.production? }
+WickedPdf.config = { :exe_path => '/usr/local/bin/wkhtmltopdf' }
