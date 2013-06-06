e71adf58efd0d772a7f1976a818610981a331997
Ryan Francis <ryanfrancis@Ryans-MacBook-Pro.local>
2013-06-06 15:32:48 -0500

    added signature to pages

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,7 +15,11 @@ class BooksController < ApplicationController
           :show_as_html => params[:debug],
           :layout => true,
           :page_height => '8.5in',
-          :page_width => '11in'
+          :page_width => '11in',
+          :margin => {:top => '1mm',
+                      :bottom => '1mm',
+                      :left => '4mm',
+                      :right => '1mm'}
       end
 
       @recipient_fb_id = @book.recipient_fb_id
