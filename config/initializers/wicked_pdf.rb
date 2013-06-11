cd9d3183d8f3284bc4e0d21fcaf58303b699580b
Ryan Francis <ryanfrancis@Ryans-MacBook-Pro.local>
2013-06-11 18:25:50 -0500

    updated initializer

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,8 +1,11 @@
 require 'wicked_pdf'
 require 'wicked_pdf_tempfile'
-if Rails.env.staging? || Rails.env.production?
-  exe_path = Rails.root.join('app','bin','wkhtmltopdf-amd64').to_s
+
+WickedPdf.config = {
+if Rails.env == "development"
+  :exe_path => Rails.root.join('app','bin','wkhtmltopdf').to_s
 else
-  exe_path = Rails.root.join('app','bin','wkhtmltopdf').to_s
+  :exe_path => Rails.root.join('app','bin','wkhtmltopdf-amd64').to_s
   # exe_path = '/usr/local/bin/wkhtmltopdf'
 end
+}
