c95f246f3d9eeb9501c4a13ae815a8cd166c2d6e
Ryan Francis <ryanfrancis@Ryans-MacBook-Pro.local>
2013-06-11 18:14:20 -0500

    added if statement for production vs development wicked pdf

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,3 +1,8 @@
 require 'wicked_pdf'
 require 'wicked_pdf_tempfile'
-WickedPdf.config = { :exe_path => Rails.root.join('app','bin','wkhtmltopdf').to_s }
+if Rails.env.staging? || Rails.env.production?
+  exe_path = Rails.root.join('app','bin','wkhtmltopdf-amd64').to_s
+else
+  exe_path = Rails.root.join('app','bin','wkhtmltopdf').to_s
+  # exe_path = '/usr/local/bin/wkhtmltopdf'
+end
