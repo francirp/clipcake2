63820e2b4a89e3b793563a85781c45db63ed9835
Ryan Francis <ryanfrancis@Ryans-MacBook-Pro.local>
2013-06-11 17:46:06 -0500

    attempting to fix wicked pdf on heroku problem which seems to be caused by bad path to wkhtmltopdf

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,3 +1,3 @@
 require 'wicked_pdf'
 require 'wicked_pdf_tempfile'
-WickedPdf.config = { :exe_path => '/usr/local/bin/wkhtmltopdf' }
+WickedPdf.config = { :exe_path => Rails.root.join('app','bin','wkhtmltopdf').to_s }
