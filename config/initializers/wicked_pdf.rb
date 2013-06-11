4dff975c2b81fbd384787edc2e184a1940a6e0d6
Ryan Francis <ryanfrancis@Ryans-MacBook-Pro.local>
2013-06-11 18:47:12 -0500

    try two

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,8 +1,6 @@
 require 'wicked_pdf'
 require 'wicked_pdf_tempfile'
 
-WickedPdf.config = {
 if Rails.env == "development"
-  :exe_path => Rails.root.join('app','bin','wkhtmltopdf').to_s
+  WickedPdf.config = { :exe_path => Rails.root.join('app','bin','wkhtmltopdf') }
 end
-}
